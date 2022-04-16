f246ce34ea9191ce472a6206aa7c9fe0b5ecd87d
chon26909 <59905927+chon26909@users.noreply.github.com>
2022-04-16 11:56:50 +0700

    update

--- src/Controllers/AuthController.ts
+++ src/Controllers/AuthController.ts
@@ -11,9 +11,9 @@ const GeneratePassword = async (password: string, salt: string) => {
     return await bcrypt.hash(password, salt);
 }
 
-const ValidatePassword = async (enterPassword: string, savedPassword: string, salt: string) => { 
+const ValidatePassword = async (enterPassword: string, savedPassword: string, salt: string) => {
 
-    return await GeneratePassword(enterPassword,salt) === savedPassword;
+    return await GeneratePassword(enterPassword, salt) === savedPassword;
 }
 
 
@@ -21,9 +21,9 @@ export const Register = async (req: Request, res: Response) => {
 
     const { email, password } = req.body;
 
-    const exist = await User.findOne({ email });
+    const existingUser = await User.findOne({ email });
 
-    if (exist) {
+    if (existingUser) {
         return res.json({ message: 'Email has already exist' })
     }
 
@@ -31,7 +31,7 @@ export const Register = async (req: Request, res: Response) => {
     const userPassword = await GeneratePassword(password, salt);
     const user = await User.create({ email, password: userPassword, salt });
 
-
+    req.session.uid = user.id;
     res.status(201).json({ user, created: true })
 }
 
@@ -41,39 +41,41 @@ export const Login = async (req: Request, res: Response) => {
         const { email, password, remember } = req.body;
         const user = await User.findOne({ email });
 
-
         if (user) {
 
             const validation = await ValidatePassword(password, user.password, user.salt);
 
             if (validation) {
-                req.session = { user }
+                req.session.uid = user.id;
                 return res.status(201).json({ loggedIn: true, user });
             }
-            else { 
-                return res.json({message: 'Password is not correct'})
+            else {
+                return res.json({ message: 'Password is not correct' })
             }
-            
+
         }
-        else { 
-            return res.json({message: 'Email not found'})
+        else {
+            return res.json({ message: 'Email not found' })
         }
-        
+
     } catch (error) {
 
     }
 }
 
 export const Logout = (req: Request, res: Response) => {
-    req.session = null;
-    return res.json({ message: 'Logout success' })
+    req.session.destroy(() => {
+        console.log(req.session)
+        res.redirect('/')
+    })
 }
 
 export const LoggedIn = (req: Request, res: Response) => {
+    console.log(req.session);
     if (req.session) {
         res.json(req.session);
     }
-    else { 
-        res.json({message: 'Session Timeout'})
+    else {
+        res.json({ message: 'Session Timeout' })
     }
 }
